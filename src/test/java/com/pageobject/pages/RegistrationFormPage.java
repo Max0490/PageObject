6e541c407618c89af973d531ea73f9e96821d30a
Max0490 <seagate90@mail.ru>
2022-09-25 15:55:54 +0300

    reg-complete

--- src/test/java/com/pageobject/pages/RegistrationFormPage.java
+++ src/test/java/com/pageobject/pages/RegistrationFormPage.java
@@ -26,13 +26,55 @@ import static com.codeborne.selenide.Selenide.open;
 
     public RegistrationFormPage openPage() {
         open("/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text(TITLE_TEXT))
+        $(".practice-form-wrapper").shouldHave(text(TITLE_TEXT));
+
+        return this
     }
+    public RegistrationFormPage setFirstName(String value) {
+        firstNameInput.setValue(value);
 
-
-
-
-
-
+        return this
     }
+     public RegistrationFormPage clearFirstName() {
+         firstNameInput.clear();
+
+         return this
+     }
+     public RegistrationFormPage setLastName(String value) {
+         lastNameInput.setValue(value);
+
+         return this
+     }
+     public RegistrationFormPage setEmail(String value) {
+         emailInput.setValue(value);
+
+         return this
+     }
+     public RegistrationFormPage setGender(String value) {
+         $("#genterWrapper").$(byText(value)).click();
+
+         return this
+     }
+     public RegistrationFormPage setNumber(String value) {
+         $("#userNumber").setValue(value);
+
+         return this
+     }
+     public RegistrationFormPage setBirthDate(String day, String month, String year) {
+         $("#dateOfBirthInput").click();
+         calendarComponent.setDate(day, month, year);
+
+         return this;
+     }
+     public RegistrationFormPage checkResultsTableVisible() {
+         resultsModal.checkVisible();
+
+         return this;
+     }
+
+     public RegistrationFormPage checkResult(String key, String value) {
+         resultsModal.checkResult(key, value);
+
+         return this;
+     }
 }
