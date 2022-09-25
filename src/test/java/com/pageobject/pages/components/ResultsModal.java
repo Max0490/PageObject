634676f29765065e1f106d02fca6a6aa45e93df0
Max0490 <seagate90@mail.ru>
2022-09-25 16:00:45 +0300

    result-complete

--- src/test/java/com/pageobject/pages/components/ResultsModal.java
+++ src/test/java/com/pageobject/pages/components/ResultsModal.java
@@ -1,6 +1,5 @@
 package com.pageobject.pages.components;
 
-import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,15 +9,15 @@ public class ResultsModal {
     private final static String TITLE_TEXT = "Thanks for submitting the form";
 
     public ResultsModal checkVisible() {
-        $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text(TITLE_TEXT));
 
         return this;
     }
-
     public ResultsModal checkResult(String key, String value) {
-        $(".table-responsive table").$(byText(key))
+        $(".table-responsive").$(byText(key))
                 .parent().shouldHave(text(value));
+
         return this;
+
     }
 }
